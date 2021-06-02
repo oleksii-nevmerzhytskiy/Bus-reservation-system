132c6472a67751e568cf9bdc2c417d95555df2a2
oleksii-nevmerzhytskiy <79115015+oleksii-nevmerzhytskiy@users.noreply.github.com>
2021-06-02 03:44:37 +0300

    Update tests.py

--- myapp/tests.py
+++ myapp/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from .models import User, Bus
 
-# Create your tests here.
+
+class BusTestCase(TestCase):
+    def test_setUp(self) -> None:
+        self.user = User.objects.create(user_id=1, name="testUser", password="1234", email="testUser@gmail.com")
+
+
+    def test_createBus(self):
+        self.bus = Bus.objects.create(bus_name="Mersedes", source="Київ", dest="Львів", nos=20, rem=20, price=300,
+                                      date="2021-06-01", time="08:30:00")
+
